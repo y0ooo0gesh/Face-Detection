8c49f2f1922049592cf59356527fcaaf1a787b5e
y0ooo0gesh <kvsyogesh703@gmail.com>
2026-05-03 21:31:00 +0530

    Updated the Model

--- src/App.jsx
+++ src/App.jsx
@@ -49,16 +49,21 @@ function App() {
   }
 
   const calculateFaceLocation = (data) =>{
-    const face = data.outputs?.[0]?.data?.regions?.[0]?.region_info?.bounding_box;
+    const regions = data.outputs?.[0]?.data?.regions;//Safety check
+    if (!regions || regions.length === 0) {
+    console.log("No faces detected.");
+    return null; 
+  }
+    const face = regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
 
     return{
-      leftcol: face.left_col * width,
-      toprow: face.top_row * height,
-      rightcol: width - (face.right_col * width),
-      bottomrow: height - (face.bottom_row * height),
+      leftCol: face.left_col * width,
+      topRow: face.top_row * height,
+      rightCol: width - (face.right_col * width),
+      bottomRow: height - (face.bottom_row * height),
     }
   }
   
